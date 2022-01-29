fb6f70374bc2d5b3704823d32f4f0366a3bdab36
vieiramauricio <mauriciorenanvs@gmail.com>
2022-01-29 17:12:19 -0300

    :sparkles: get data from prismic
    get data from prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import Header from "../components/Header";
-// import { GetStaticProps } from 'next';
+import { GetStaticProps } from 'next';
+import Prismic from '@prismicio/client';
 
-// import { getPrismicClient } from '../services/prismic';
+import { getPrismicClient } from '../services/prismic';
 
 // import commonStyles from '../styles/common.module.scss';
 // import styles from './home.module.scss';
@@ -26,11 +27,22 @@ import Header from "../components/Header";
 // }
 
 export default function Home() {
-  return (<Header />)
+  return <Header />
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'po')],
+    {
+      fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+      pageSize: 2,
+    }
+  );
+
+  return {
+    props: {
+      postsPagination: postsResponse
+    }
+  }
+};
