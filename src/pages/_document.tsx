de6c327573ef67ade9b8e4b32d695c176ada0312
vieiramauricio <mauriciorenanvs@gmail.com>
2022-01-29 16:33:59 -0300

    :tada: init project and clean structure
    init project and clean structure

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,28 @@
-import Document from 'next/document';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 
-export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default MyDocument
